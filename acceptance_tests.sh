f453a53b5a458f395ca9ff98e661910adc1200fc
Ukasz09 <246703@student.pwr.edu.pl>
2020-06-02 12:20:28 +0200

    Upgrade acceptance tests API

--- acceptance_tests.sh
+++ acceptance_tests.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+APP_PATH=${1}
 PASSED_TESTS_QTY=0
 FAILED_TESTS_QTY=0
+PASS_MSG="OK"
+FAILED_MSG="ERROR"
 
-#########################################################################################################
+###############################################################################################################################
 # API
-#########################################################################################################
+###############################################################################################################################
 retVal=""
 
 doTest(){
     name=${1}
     expected=${2}
-    cmd="./app.sh ${@:3}"
+    Cmd="${APP_PATH} ${@:3}"
     actual="$($cmd)"
     actual_trimmed="$(echo -e "${actual}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
     
@@ -23,44 +26,42 @@ checkTestResult(){
     excepted=${2}
     actual=${3}
     if [ "${expected}" = "${actual}" ]; then
-        retVal="PASSED"
+        retVal="\e[1;47m\e[1;30m PASSED \e[0m"
         ((PASSED_TESTS_QTY++))
     else
-        retVal="FAILED"
+        retVal="\e[1;41m FAILED \e[0m"
         ((FAILED_TESTS_QTY++))
     fi
 }
 
 printTestInfo(){
-    echo "_______________________________________"
+    echo "________________________________________________________________________________________________________"
     echo "Name: ${1}"
     echo "cmd: ${@:4}"
     echo "Expected: ${2}"
     echo "Actual: ${3}"
-    echo "Result: ${retVal}"
-    echo "_______________________________________"
+    echo -e "Result: ${retVal}"
+    echo "________________________________________________________________________________________________________"
 }
 
 printTestsSummary(){
-    echo "#-----------------------------------------------#"
+    echo "#-------------------------------------------------------------------------------------------------------#"
     allTestsQty=$((FAILED_TESTS_QTY+PASSED_TESTS_QTY))
-    passedPercents=$((PASSED_TESTS_QTY/allTestsQty*100))
+    passedPercents=$(echo "scale=2; ${PASSED_TESTS_QTY}/${allTestsQty}*100" | bc)
     echo "DONE TESTS QTY: ${allTestsQty}"
     echo "FAILED TESTS QTY: ${FAILED_TESTS_QTY}"
     echo "PASSED TESTS QTY: ${PASSED_TESTS_QTY}"
     echo "PASSED TESTS: ${passedPercents} %"
-    echo "#-----------------------------------------------#"
+    echo "#-------------------------------------------------------------------------------------------------------#"
 }
 
-#########################################################################################################
+#############################################################################################################################
 # TESTS
-#########################################################################################################
-givenEmptyCollectionWhenAddThenAdded() {
-    name="givenEmptyCollectionWhenAddThenAdded"
-    expected="OK"
-    cmd="add student John Carter"
-    doTest $name $expected $cmd
-}
+#############################################################################################################################
+doTest "givenEmptyCollectionWhenAddCorrectStudentThenAdded" ${PASS_MSG} "add student John Carter"
+doTest "given1StudentInCollectionWhenAddTheSameStudentThenNotAdded" ${FAILED_MSG} "add student John Carter"
+doTest "givenNotEmptyCollectionWhenAddStudentWithOnlyTheSameNameThenAdded" ${PASS_MSG} "add student John Dicaprio"
+doTest "givenNotEmptyCollectionWhenAddStudentWithTheOnlySameSurnameThenAdded" ${PASS_MSG} "add student Eleonore Carter"
+doTest "givenNotEmptyCollectionWhenAddDifferentStudentThenAdded" ${PASS_MSG} "add student Penelope Cruze"
 
-givenEmptyCollectionWhenAddThenAdded
 printTestsSummary
